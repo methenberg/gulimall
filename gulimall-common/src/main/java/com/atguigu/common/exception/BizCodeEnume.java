26e069f305b8a16951ac6aa5f55646bde71a0a2b
methenberg <541438722@qq.com>
2021-03-23 18:51:02 -0400

    用户注册登录功能

--- gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
+++ gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
@@ -3,7 +3,11 @@ package com.atguigu.common.exception;
 public enum BizCodeEnume {
     UNKOWNM_EXCEPTION(10000,"系统未知异常"),
     VALID_EXCEPTION(10001,"参数格式校验失败"),
-    PRODUCT_UP_EXCEPTION(11000,"商品上架异常");
+    VALID_SMS_CODE(10002,"短信验证码获取频率太高"),
+    PRODUCT_UP_EXCEPTION(11000,"商品上架异常"),
+    USER_EXIST_EXCEPTION(15001,"用户已被注册"),
+    PHONE_EXIST_EXCEPTION(15002,"手机已被注册"),
+    LOGINACCT_PASSWORD_INVALID_EXCEPTION(15003,"账号密码错误");
 
     private int code;
     private String  msg;
--- gulimall-member/src/main/java/com/atguigu/gulimall/member/service/impl/MemberServiceImpl.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/service/impl/MemberServiceImpl.java
@@ -1,5 +1,13 @@
 package com.atguigu.gulimall.member.service.impl;
 
+import com.atguigu.gulimall.member.dao.MemberLevelDao;
+import com.atguigu.gulimall.member.entity.MemberLevelEntity;
+import com.atguigu.gulimall.member.exception.PhoneExistException;
+import com.atguigu.gulimall.member.exception.UsernameExistException;
+import com.atguigu.gulimall.member.vo.MemberLoginVo;
+import com.atguigu.gulimall.member.vo.MemberRegistVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -16,6 +24,9 @@ import com.atguigu.gulimall.member.service.MemberService;
 @Service("memberService")
 public class MemberServiceImpl extends ServiceImpl<MemberDao, MemberEntity> implements MemberService {
 
+    @Autowired
+    MemberLevelDao memberLevelDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<MemberEntity> page = this.page(
@@ -24,6 +35,75 @@ public class MemberServiceImpl extends ServiceImpl<MemberDao, MemberEntity> impl
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public void regist(MemberRegistVo vo) {
+        MemberDao memberDao = this.baseMapper;
+        MemberEntity entity = new MemberEntity();
+
+        //设置默认等级
+        MemberLevelEntity levelEntity = memberLevelDao.getDefaultLevel();
+        entity.setLevelId(levelEntity.getId());
+
+        //检查用户名和手机号是否唯一,异常机制
+        checkPhoneUnique(vo.getPhone());
+        checkUsernameUnique(vo.getUserName());
+
+        entity.setMobile(vo.getPhone());
+        entity.setUsername(vo.getUserName());
+        //加密存储密码
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encode = passwordEncoder.encode(vo.getPassword());
+        entity.setPassword(encode);
+        //其余默认信息
+
+        //保存
+        memberDao.insert(entity);
+    }
+
+    @Override
+    public void checkPhoneUnique(String phone) throws PhoneExistException {
+        MemberDao memberDao = this.baseMapper;
+        Integer mobile = memberDao.selectCount(new QueryWrapper<MemberEntity>().eq("mobile", phone));
+        if(mobile>0){
+            throw new PhoneExistException();
+        }
+
+    }
+
+    @Override
+    public void checkUsernameUnique(String username) throws UsernameExistException {
+        MemberDao memberDao = this.baseMapper;
+        Integer count = memberDao.selectCount(new QueryWrapper<MemberEntity>().eq("username", username));
+        if(count>0){
+            throw new UsernameExistException();
+        }
+
+    }
+
+    @Override
+    public MemberEntity login(MemberLoginVo vo) {
+        String loginacct = vo.getLoginacct();
+        String password = vo.getPassword();
+
+        //1、去数据库查询
+        MemberDao memberDao = this.baseMapper;
+        MemberEntity entity = memberDao.selectOne(new QueryWrapper<MemberEntity>().eq("username", loginacct).or().eq("mobile", loginacct));
+        if(entity == null){
+            //数据库无用户
+            return null;
+        }else {
+            String password1 = entity.getPassword();
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            //明文密文匹配
+            boolean matches = encoder.matches(password, password1);
+            if (matches){
+                return  entity;
+            }else {
+                return null;
+            }
+        }
     }
 
 }
